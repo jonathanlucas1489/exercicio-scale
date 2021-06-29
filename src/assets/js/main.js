0704ac0eaf8c681101c221ff8eae638a3ffaacc5
jonathanlucas1489 <jonathan.morais@ccc.ufcg.edu.br>
2021-06-29 19:43:07 -0300

    Final

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,7 @@
 $('.first_challenger').on('click', (e) => {
     e.preventDefault()
     $.ajax('https://reqres.in/api/users', {
-        success: function (data, status, xhr) {
+        success: function (data) {
             $(".content").empty();
             listUsers(data.data)
         }
@@ -11,23 +11,39 @@ $('.first_challenger').on('click', (e) => {
 $('.second_challenger').on('click', (e) => {
     e.preventDefault()
     $.ajax('http://www.amock.io/api/fcmaia/countries', {
-        success: function (data, status, xhr) {
+        success: function (data) {
             $(".content").empty();
-            console.log(data)
+                moreBorder(data)
         }
-        })
+    })
 })
 
 function listUsers(data) {
     data.map((user) => {
-        console.log(user)
-        const avatarImg = $('<img>').attr('src', user.avatar)
+        const avatarImg = $('<img>').attr('src', user.avatar).addClass('picture')
         const avatar = $('<div>').append(avatarImg)
-        const id = $('<div>').text(user.id)
-        const first_name = $('<div>').text(user.first_name)
-        const last_name = $('<div>').text(user.last_name)
-        const email = $('<div>').text(user.email)
-        const displayUser = $('<div>').append(avatar, id, first_name, last_name, email).addClass('avatar')
+        const email = $('<div>').text(`Email: ${user.email}`).attr('id', 'userEmail')
+        const UserInfo = $('<div>').text(`${user.first_name} ${user.last_name}`).addClass('user').append(email)
+        const displayUser = $('<div>').append(avatar, UserInfo).addClass('avatar')
         $(".content").append(displayUser)
     })
+}
+
+function moreBorder(countries) {
+    if(finishOrder(countries)) {
+        const biggest = countries.reduce((prev, country) => {
+            return (prev.fronteiras.length > country.fronteiras.length) ? prev : country
+        })
+        countries.map((country) => country.code === biggest.code ?  country.fronteiras = [] : country)
+        const country = $('<div>').text(`${biggest.code} ${biggest.name}`).addClass('country')
+        $(".content").append(country)
+        return moreBorder(countries)
+    }
+    return countries
+}
+
+function finishOrder(countries) {
+    let result = false
+    countries.forEach((country) => country.fronteiras.length > 0 ? result = true : result = result)
+    return result
 }
